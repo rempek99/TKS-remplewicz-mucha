c26c0d4d81fa2288a4b1bda9ca88a074deee04bb
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-03-10 21:13:34 +0100

    Rest Activation
    Application seems to work ok.
    Access to sample data by restapi works.
    Security Constrains in web.xml are still commented
    'java.net.ConnectException: Connection refused: connect' trying to get to /whole/list*.xhtml

--- ApplicationCore/src/main/java/rest/JaxRsActivator.java
+++ ApplicationCore/src/main/java/rest/JaxRsActivator.java
@@ -1,17 +1,17 @@
-//package rest;
-//
-//import javax.enterprise.context.ApplicationScoped;
-//import javax.ws.rs.ApplicationPath;
-//import javax.ws.rs.core.Application;
-//import java.util.Set;
-//
+package rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
 //@ApplicationScoped
-//@ApplicationPath("/rest")
-//public class JaxRsActivator extends Application {
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
 //    @Override
 //    public Set<Class<?>> getClasses() {
 //        Set<Class<?>> resources = new java.util.HashSet<>();
 //        resources.add(RestEndpoints.class);
 //        return resources;
 //    }
-//}
+}
