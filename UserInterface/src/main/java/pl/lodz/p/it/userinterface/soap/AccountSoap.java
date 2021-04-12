cb3da3a47626b15181132f8f4223d5da3d085813
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-04-12 15:02:01 +0200

    Testowa klasa, zmiana root Context aplikacji

--- UserInterface/src/main/java/pl/lodz/p/it/userinterface/soap/AccountSoap.java
+++ UserInterface/src/main/java/pl/lodz/p/it/userinterface/soap/AccountSoap.java
@@ -1,10 +1,12 @@
 package pl.lodz.p.it.userinterface.soap;
 
 import pl.lodz.p.it.viewadapters.adapters.AccountServiceAdapter;
+import pl.lodz.p.it.viewmodel.modelDTO.AccountDTO;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jws.WebService;
+import java.util.List;
 
 @WebService(serviceName = "AccountAPI")
 public class AccountSoap {
@@ -12,7 +14,11 @@ public class AccountSoap {
     @Inject
     AccountServiceAdapter accountAdapter;
 
+
     public String hello() {
         return "Hello Soap";
+    }
+    public List<AccountDTO> getAccounts() {
+        return accountAdapter.getAllAccounts();
     }
 }
